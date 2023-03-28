f0caa1e62b18d9faee4be4a6b2be2adc46558bb7
AnandKumar1008 <anandkumar031199@gmail.com>
2023-03-28 08:19:23 +0530

    date time were added correctly

--- weather.js
+++ weather.js
@@ -13,18 +13,26 @@ const getWeather = (city) => {
         .then(response => {
             console.log(response)
             // cloud_pct.innerHTML = response.cloud_pct
-            temp.innerHTML = response.temp
-            temp2.innerHTML = response.temp
-            feels_like.innerHTML = response.feels_like
-            humidity.innerHTML = response.humidity
-            humidity2.innerHTML = response.humidity
-            min_temp.innerHTML = response.min_temp
-            max_temp.innerHTML = response.max_temp
-            wind_speed.innerHTML = response.wind_speed
-            wind_speed2.innerHTML = response.wind_speed
-            wind_degrees.innerHTML = response.wind_degrees
-            sunrise.innerHTML = response.sunrise
-            sunset.innerHTML = response.sunset
+            temp.innerHTML = `${response.temp}\u00B0C`;
+            temp2.innerHTML = response.temp;
+            feels_like.innerHTML = `${response.feels_like}\u00B0C`;
+            humidity.innerHTML = `${response.humidity}%`;
+            humidity2.innerHTML = response.humidity;
+            min_temp.innerHTML = `${response.min_temp}\u00B0C`;
+            max_temp.innerHTML = `${response.max_temp}\u00B0C`;
+            wind_speed.innerHTML =`${ response.wind_speed}\u00B0`;
+            wind_speed2.innerHTML = `${response.wind_speed}km/hr`;
+            wind_degrees.innerHTML = response.wind_degrees;
+            const sunRise=new Date(response.sunrise*1000);
+            let date=sunRise.toString();
+        let time=date.split(" ");
+            sunrise.innerHTML = time[4];
+            const sunSet=new Date(response.sunset*1000);
+            date=sunSet.toString();
+        time=date.split(" ");
+           
+
+            sunset.innerHTML = time[4];
 
 
         })
@@ -63,15 +71,26 @@ function myFunction(){
         const td9=document.createElement('td');
         const td10=document.createElement('td');
         th.innerText=city;
-        td1.innerText=response.temp;
-        td2.innerText=response.feels_like;
-        td3.innerText=response.humidity;
-        td4.innerText=response.max_temp;
-        td5.innerText=response.min_temp;
-        td6.innerText=response.sunrise;
-        td7.innerText=response.sunset;
-        td8.innerText=response.wind_degrees;
-        td9.innerText=response.wind_speed;
+        td1.innerText=`${response.temp}\u00B0C`;
+        td2.innerText=`${response.feels_like}\u00B0C`;
+        td3.innerText=`${response.humidity}%`;
+        td4.innerText=`${response.max_temp}\u00B0C`;
+        td5.innerText=`${response.min_temp}\u00B0C`;
+
+        const sunRise=new Date(response.sunrise*1000);
+        let date=sunRise.toString();
+        let time=date.split(" ");
+        td6.innerText=time[4];
+        const sunSet=new Date(response.sunset*1000);
+        console.log(sunRise);
+        
+         date=sunSet.toString();
+         time=date.split(" ");
+
+        
+        td7.innerText=time[4];
+        td8.innerText=`${response.wind_degrees}\u00B0`;
+        td9.innerText=`${response.wind_speed}km/hr`;
         td10.innerText=response.cloud_pct;
         element.innerHTML='';
         element.appendChild(th);
